1e67d7cfcb8a18736035bece1ebdcd94202db5f0
YamidBarrera <yamidebs@gmail.com>
2025-03-05 09:26:28 -0500

    primeros endpoits

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import "dotenv/config.js";
+
+const server = express();
+
+const PORT = process.env.PORT || 8080;
+
+const ready = () => console.log("Server is running on port: " + PORT);
+
+server.listen(PORT, ready);
